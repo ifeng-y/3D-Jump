27a9bc8358cbe947201937f75cb900dea307787b
yaofeng <ifeng.y@foxmail.com>
2023-06-16 20:56:20 +0800

    Initial commit

---
+++ assets/Scripts/GameManger.ts
@@ -0,0 +1,201 @@
+import { _decorator, Component, Prefab, instantiate, Node, Label, CCInteger, Vec3 } from 'cc';
+import { PlayerController } from "./PlayerController";
+const { ccclass, property } = _decorator;
+
+// 赛道格子类型，坑（BT_NONE）或者实路（BT_STONE）
+enum BlockType{
+    BT_NONE,
+    BT_STONE,
+};
+
+enum GameState{
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
+    GS_FAIL,
+};
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+
+    // 赛道预制
+    @property({type: Prefab})
+    public cubePrfb: Prefab | null = null;
+    // 赛道长度
+    @property({type: CCInteger})
+    public roadLength: number = 50;
+    private _road: BlockType[] = [];
+    // 主界面根节点
+    @property({type: Node})
+    public startMenu: Node | null = null;
+    // 失败页面根节点
+    @property({type: Node})
+    public failMenu: Node | null = null;
+    // 成功页面根节点
+    @property({type: Node})
+    public successMenu: Node | null = null;
+    // 关联 Player 节点身上 PlayerController 组件
+    @property({type: PlayerController})
+    public playerCtrl: PlayerController | null = null;
+    // 关联步长文本组件
+    @property({type: Label})
+    public stepsLabel: Label | null = null!;
+
+    start () {
+        this.curState = GameState.GS_INIT;
+        this.playerCtrl?.node.on('JumpEnd', this.onPlayerJumpEnd, this);
+    }
+
+    init() {
+        if (this.failMenu || this.successMenu) {
+            this.failMenu.active = false;
+            this.successMenu.active = false;
+        }
+        // 激活主界面
+        if (this.startMenu) {
+            this.startMenu.active = true;
+        }
+        // 生成赛道
+        this.generateRoad();
+        if(this.playerCtrl){
+            // 禁止接收用户操作人物移动指令
+            this.playerCtrl.setInputActive(false);
+            // 重置人物位置
+            this.playerCtrl.node.setPosition(Vec3.ZERO);
+            // 重置已经移动的步长数据
+            this.playerCtrl.reset();
+        }
+    }
+
+    set curState (value: GameState) {
+        switch(value) {
+            case GameState.GS_INIT:
+                this.init();
+                break;
+            case GameState.GS_PLAYING: 
+                if (this.startMenu) {
+                    this.startMenu.active = false;
+                }
+
+                if (this.stepsLabel) {
+                    this.stepsLabel.string = '0';   // 将步数重置为0
+                }
+                // 会出现的现象就是，游戏开始的瞬间人物已经开始移动
+                // 因此，这里需要做延迟处理
+                setTimeout(() => { 
+                    if (this.playerCtrl) {
+                        this.playerCtrl.setInputActive(true);
+                    }
+                }, 0.1);
+                break;
+            case GameState.GS_END:
+                if (this.successMenu) {
+                    this.successMenu.active = true;
+                }
+                break;
+            case GameState.GS_FAIL:
+                if (this.failMenu) {
+                    this.failMenu.active = true;
+                }
+                break;
+        }
+    }
+
+    generateRoad() {
+        // 防止游戏重新开始时，赛道还是旧的赛道
+        // 因此，需要移除旧赛道，清除旧赛道数据
+        this.node.removeAllChildren();
+        this._road = [];
+        // 确保游戏运行时，人物一定站在实路上
+        this._road.push(BlockType.BT_STONE);
+
+        // 确定好每一格赛道类型
+        for (let i = 1; i < this.roadLength - 1; i++) {
+            // 如果上一格赛道是坑，那么这一格一定不能为坑
+            if (this._road[i-1] === BlockType.BT_NONE) {
+                this._road.push(BlockType.BT_STONE);
+            } else {
+                this._road.push(Math.floor(Math.random() * 2));
+            }
+        }
+        // 最后一格也是石头
+        this._road.push(BlockType.BT_STONE);
+
+        // 根据赛道类型生成赛道
+        let linkedBlocks = 0;
+        for (let j = 0; j < this._road.length; j++) {
+            if(this._road[j]) {
+                ++linkedBlocks;
+            }
+            if(this._road[j] == 0) {
+                if(linkedBlocks > 0) {
+                    this.spawnBlockByCount(j - 1, linkedBlocks);
+                    linkedBlocks = 0;
+                }
+            }        
+            if(this._road.length == j + 1) {
+                if(linkedBlocks > 0) {
+                    this.spawnBlockByCount(j, linkedBlocks);
+                    linkedBlocks = 0;
+                }
+            }
+        }
+    }
+
+    spawnBlockByCount(lastPos: number, count: number) {
+        let block: Node|null = this.spawnBlockByType(BlockType.BT_STONE);
+        if(block) {
+            this.node.addChild(block);
+            block?.setScale(count, 1, 1);
+            block?.setPosition(lastPos - (count - 1) * 0.5, -1.5, 0);
+        }
+    }
+    spawnBlockByType(type: BlockType) {
+        if (!this.cubePrfb) {
+            return null;
+        }
+
+        let block: Node|null = null;
+        switch(type) {
+            case BlockType.BT_STONE:
+                block = instantiate(this.cubePrfb);
+                break;
+        }
+
+        return block;
+    }
+
+    onStartButtonClicked() {
+        // 点击主界面 play 按钮，开始游戏
+        this.curState = GameState.GS_PLAYING;
+    }
+
+    playAgain(){
+        this.curState = GameState.GS_INIT;
+    }
+
+    checkResult(moveIndex: number) {
+        if (moveIndex < this.roadLength - 1) {
+            // 跳到了坑上
+            if (this._road[moveIndex] == BlockType.BT_NONE) {
+                this.curState = GameState.GS_FAIL;
+                // 禁止接收用户操作人物移动指令
+                this.playerCtrl.setInputActive(false);
+            }
+        } else {    
+            // 跳过了最大长度
+            this.curState = GameState.GS_END;
+            // 禁止接收用户操作人物移动指令
+            this.playerCtrl.setInputActive(false);
+        }
+    }
+
+    onPlayerJumpEnd(moveIndex: number) {
+        if (this.stepsLabel) {
+            // 因为在最后一步可能出现步伐大的跳跃，但是此时无论跳跃是步伐大还是步伐小都不应该多增加分数
+            this.stepsLabel.string = '' + (moveIndex >= this.roadLength ? this.roadLength : moveIndex);
+        }
+        // 检查当前下落道路的类型，获取结果
+        this.checkResult(moveIndex);
+    }
+}
---
+++ assets/Scripts/PlayerController.ts
@@ -0,0 +1,123 @@
+import { _decorator, Component, Vec3, input, Input, EventMouse, Animation, SkeletalAnimation, EventTouch, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass("PlayerController")
+export class PlayerController extends Component {
+
+    @property(Node)
+    leftTouch: Node = null;
+
+    @property(Node)
+    rightTouch: Node = null;
+
+    @property({type: Animation})
+    public BodyAnim: Animation|null = null;
+    // @property({type: SkeletalAnimation})
+    // public CocosAnim: SkeletalAnimation|null = null;
+
+    // for fake tween
+    private _startJump: boolean = false;
+    private _jumpStep: number = 0;
+    private _curJumpTime: number = 0;
+    private _jumpTime: number = 0.3;
+    private _curJumpSpeed: number = 0;
+    private _curPos: Vec3 = new Vec3();
+    private _deltaPos: Vec3 = new Vec3(0, 0, 0);
+    private _targetPos: Vec3 = new Vec3();
+    private _curMoveIndex = 0;
+
+    start () {
+    }
+
+    reset() {
+        this._curMoveIndex = 0;
+    }
+
+    setInputActive(active: boolean) {
+        // if (active) {
+        //     input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        // } else {
+        //     input.off(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        // }
+
+        if (active) {    
+            this.leftTouch.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+            this.rightTouch.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+        } else {        
+            this.leftTouch.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
+            this.rightTouch.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
+        }
+    }
+
+    onTouchStart(event: EventTouch) {
+      const target = event.target as Node;    
+      if (target?.name == 'LeftTouch') {
+          this.jumpByStep(1);
+      } else {
+          this.jumpByStep(2);
+      }
+  }
+
+    // onMouseUp(event: EventMouse) {
+    //     if (event.getButton() === 0) {
+    //         this.jumpByStep(1);
+    //     } else if (event.getButton() === 2) {
+    //         this.jumpByStep(2);
+    //     }
+    // }
+
+    jumpByStep(step: number) {
+        if (this._startJump) {
+            return;
+        }
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep / this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep, 0, 0));
+
+        // if (this.CocosAnim) {
+        //     this.CocosAnim.getState('cocos_anim_jump').speed = 3.5; //跳跃动画时间比较长，这里加速播放
+        //     this.CocosAnim.play('cocos_anim_jump'); //播放跳跃动画
+        // }
+
+        if (this.BodyAnim) {
+            if (step === 1) {
+                // this.BodyAnim.getState('oneStep').speed = 5;
+                this.BodyAnim.play('oneStep');
+            } else if (step === 2) {
+                // this.BodyAnim.getState('twoStep').speed = 5;
+                this.BodyAnim.play('twoStep');
+            }
+        }
+
+        this._curMoveIndex += step;
+    }
+
+    onOnceJumpEnd() {
+        // if (this.CocosAnim) {
+        //     this.CocosAnim.play('cocos_anim_idle');
+        // }
+
+        this.node.emit('JumpEnd', this._curMoveIndex);
+    }
+
+    update (deltaTime: number) {
+        if (this._startJump) {
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
+                // end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+                this.onOnceJumpEnd();
+            } else {
+                // tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
+        }
+    }
+}
